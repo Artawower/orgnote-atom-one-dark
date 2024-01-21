72261d0d8316cb2165ae7786f3f4fedea4bbc06a
artawower <artawower@mail.ru>
2024-01-21 04:47:06 +0300

    add base7 color

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,28 @@
 export default {
   onMounted: async (api) => {
     api.ui.applyTheme({
-      blue: '#61afef',
-      red: '#e06c75',
-      bg: '#282c34',
-      bgAlt: '#21252b',
-      fg: '#abb2bf',
-      white: '#abb2bf',
-      green: '#98c379',
-      orange: '#d19a66',
-      yellow: '#e5c07b',
-      violet: '#c678dd',
-      cyan: '#56b6c2',
+      blue: "#61afef",
+      red: "#e06c75",
+      bg: "#282c34",
+      bgAlt: "#21252b",
+      fg: "#abb2bf",
+      white: "#abb2bf",
+      green: "#98c379",
+      orange: "#d19a66",
+      yellow: "#e5c07b",
+      violet: "#c678dd",
+      cyan: "#56b6c2",
+      base7: "rgba(99, 110, 123, 0.4)",
     });
   },
 };
 
 export const manifest = {
-  category: 'theme',
-  name: 'Atom One Dark Theme',
-  apiVersion: '0.13.4',
-  version: '0.0.1',
-  description: 'Atom One Dark theme for OrgNote!',
-  keywords: ['ui', 'theme'], //
-  repo: 'https://github.com/artawower/orgnote-atom-one-dark',
+  category: "theme",
+  name: "Atom One Dark Theme",
+  apiVersion: "0.13.4",
+  version: "0.0.1",
+  description: "Atom One Dark theme for OrgNote!",
+  keywords: ["ui", "theme"], //
+  repo: "https://github.com/artawower/orgnote-atom-one-dark",
 };
